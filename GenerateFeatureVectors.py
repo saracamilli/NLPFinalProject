06edb2f540bcd20be9c0c10ff0e00370cffc3223
Palmer Robins <william.robins@richmond.edu>
2019-04-29 17:27:44 -0400

    Edited parser and created main.py

--- GenerateFeatureVectors.py
+++ GenerateFeatureVectors.py
@@ -6,20 +6,19 @@ import codecs
 from nltk.tokenize import PunktSentenceTokenizer,sent_tokenize, word_tokenize
 from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer, PorterStemmer
+nltk.download('wordnet')
 
 #################################################################################################################
-# Generate feature vectors using n-gram counts and keywords (using helpers), then returns these feature vectors 
+# Generate feature vectors using n-gram counts and keywords (using helpers), then returns these feature vectors
 # as a list. These will be used to create the arff file to feed to weka's machine learning classifier
 #################################################################################################################
-def generateTrainingFeatureVectors(entries):
+def generateTrainingFeatureVectors(countryEntries, hiphopEntries):
 
     # Lists of all the training feature vectors for country and hip hop (consists of both n-gram features
     # and keyword features)
     # TODO: is this the way to go? I kinda forget how this is supposed to work, please help
-    allCountryVectors = []
-    allHipHopVectors = []
 
-    # Loop through all entries 
+    # Loop through all entries
     for entry in entries:
 
         # Get n-gram features
@@ -59,9 +58,9 @@ def getNGramFeatures(textBlock):
 # Output: count of n-word sequences
 #################################################################################################################
 def ngramCounts(textBlock, n):
-    
+
     myCountDict = {}                        # will hold all the n-grams and their counts
-                                                    
+
     # Loop through and find all the possible n-grams, adding them
     # to its dictionary. If it already exists in the dictionary, update
     # its count
@@ -74,7 +73,7 @@ def ngramCounts(textBlock, n):
         currNGram.append(filter(word))      # filter the word and add it to current n-gram
 
         # If the end of the file has not yet been reached...
-        if (counter + n <= len(textBlock)):                 
+        if (counter + n <= len(textBlock)):
             endIndex = counter + n
             for theNext in range(counter+1, endIndex):        # loop through next n-1 subsequent words
                 currNGram.append(textBlock[theNext])          # put all these words together into an n-gram
@@ -82,7 +81,7 @@ def ngramCounts(textBlock, n):
             currNGramStr.replace("[","").replace("]","").replace("'","")
 
             flag = 0                            # flag for whether we've seen this n-gram before
-            for key in myCountDict:              # loop through all n-grams in dictionary                     
+            for key in myCountDict:              # loop through all n-grams in dictionary
                 if (key == currNGramStr):                   # if we've encountered this n-gram before...
                     currCount = myCountDict[currNGramStr]      # find it's current count
                     myCountDict.update({currNGramStr : currCount + 1})   # update the dictionary with count + 1
@@ -90,15 +89,15 @@ def ngramCounts(textBlock, n):
                     break
 
             if flag == 0:
-                myCountDict.update({currNGramStr : 1})        # otherwise, add new entry with count 1             
+                myCountDict.update({currNGramStr : 1})        # otherwise, add new entry with count 1
 
-            counter = counter + 1                   
+            counter = counter + 1
 
     # Return this dictionary with all n-grams and their counts
     return myCountDict
 
 #################################################################################################################
-# Given a text block, extracts presence of keywords for both hip hop and country features and creates two 
+# Given a text block, extracts presence of keywords for both hip hop and country features and creates two
 # feature vectors which it returns as (hip hop vector, country vector)
 # TODO: should we change this to frequency, or is presence good enough?
 #################################################################################################################
@@ -108,13 +107,13 @@ def extractKeywordFeatures(textBlock):
     hipHopKeywords = ["chopper", "stunting", "flexing", "mane", "trill", "trapping", "balling" \
         "realest", "homie", "snitch", "biggie", "grind", "nigga", "shit", "bitch", "skrrt", \
             "never", "fuck", "hit", "money", "ass", "big", "real"]
-    
+
     countryKeywords = ["ride", "baby", "oh", "tobacco", "windows", "blown", \
         "road", "memory", "windows", "drunk", "got", "know", "highway", "cold", "beer" \
             "little", "away", "dirt", "town", "chew", "whoa", "plane", "southern", "south" \
                 "redneck", "springsteen", "cruise", "truck", "headlights", "town" \
                     "radio", "hey", "rolling", "song", "round", "til", "lane", "wind"]
-    
+
     # Define and initialize feature vectors with 0's
     hipHopFeatureVect = []
     for i in range(len(hipHopKeywords)): hipHopFeatureVect.append(0)
@@ -122,7 +121,7 @@ def extractKeywordFeatures(textBlock):
     for i in range(len(countryKeywords)): countryFeatureVect.append(0)
 
     # Update the feature vectors
-        # NOTE: the words are not filtered because, esp. in the case of hip hop, the 
+        # NOTE: the words are not filtered because, esp. in the case of hip hop, the
         # suffixes can be meaningful (i.e. balling, not ball)
     for word in textBlock:
         # Check if the current word is in hip hop keywords list
